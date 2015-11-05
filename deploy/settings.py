aca491b9654b5cdab642b49c56090a3e4b8071f3
Ali Bozorgkhan <alibozorgkhan@gmail.com>
2015-11-04 18:35:52 -0800

    test

--- deploy/settings.py
+++ deploy/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'deploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'deploy',
+        'USER': 'vlab',
+        'PASSWORD': '7pVx94OMc3FRHw4MNFXQuvSou7U7SzZ6uBO70e8adXCI5skk5rcWkk2i1583b1Hb',
+        'HOST': 'aaunm0ug0b2qu8.cm8d9baz99cn.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
